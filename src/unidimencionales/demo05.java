2d741d519abf570f2b88fd61c8083586ccd1be7f
gersha-code <gershita.cafeina.pop@gmail.com>
2024-11-06 01:38:39 -0600

    third  commit

--- src/unidimencionales/demo05.java
+++ src/unidimencionales/demo05.java
@@ -15,15 +15,40 @@ public class demo05 {
     public static void main(String[] args) {
         Random random = new Random();
         int number = random.nextInt(1000, 9999);
-        System.out.println(number);
-        String string_number = String.valueOf(number);
-        int vector_1 [] = new int[string_number.length()];
-        System.out.print("Primer arreglo: | ");
-        for (int i = 0; i < string_number.length(); i++) {
-            
-            System.out.print(vector_1[i] + " | ");
+        int vector[] = new int[String.valueOf(number).length()];
+        for (int i = 0; i < String.valueOf(number).length(); i++) {
+            vector[i] = String.valueOf(number).charAt(i) - '0';
         }
-        
+        /*
+         * Con String,valueOf(number).charAt(i) se obtine el valor ASCII
+         * Una manera de obtener el valor real es restando otro ASCII,
+         * el valor '0' es 46 en ASCII, esto funcionaria para obtener los
+         * valores numericos de acuerdo a la tabla ASCII.
+         */
+        System.out.print("Valor aleatorio: | ");
+        for (int i = 0; i < vector.length; i++) {
+            System.out.print(vector[i] + " | ");
+        }
+
+        // C I F R A D O
+        System.out.print("\nCifrado 1: | ");
+        for (int i = 0; i < vector.length; i++) {
+            vector[i] = (vector[i] + 7) % 10;
+            System.out.print(vector[i] + " | ");
+        }
+        int vector_cifrado [] = new int[vector.length];
+        for (int i = 0; i < vector.length; i++) {
+            if (i % 2 == 0) { // es impar
+                vector_cifrado[i] = vector[(vector.length / 2) - i];
+            } else {
+                vector_cifrado[i] = vector[vector.length - i];
+            }
+        }
+        System.out.print("\nCifrado 2: |" );
+        for (int i = 0; i < vector.length; i++) {
+            System.out.print(vector_cifrado[i] + " | ");
+        }
+
     }
 
 }
