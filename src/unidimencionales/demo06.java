e6722a6a4a7b19df2131fc08d43aca5908719154
gersha-code <gershita.cafeina.pop@gmail.com>
2024-11-06 17:59:16 -0600

    4  commit

--- src/unidimencionales/demo06.java
+++ src/unidimencionales/demo06.java
@@ -1,5 +1,15 @@
 package unidimencionales;
+/*
+ * Llenar un arreglo unidimencional de N celdas, con valores aleatorios entre 3 y 900, cuidando que los valores queden en orden ascendente.
+ * El valor de la celda 1 debe ser mayor o igual que el de la celda 0, el valor de la celda 2 debe ser mayor o igual que el de la celda 1, etc.
+ * Mostrar el arreglo. N es un valor ingresado por el usuario y debe validarse que sea mayor que 1 y menor que 50. Observe el ejemplo, suponiendo
+ * que N vale 8. |3|7|9|12|25|25|60|500|
+ *
+ */
 
 public class demo06 {
+    public static void main(String[] args) {
+        
+    }
 
 }
